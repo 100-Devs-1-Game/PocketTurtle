05fd1f26d3b31fb9844745cb21834a69ab291818
TateyamaYuki <150481042+TateyamaYuki@users.noreply.github.com>
2025-12-18 14:11:20 -0800

    Adds rudimentary credits (#36)

--- src/game/settings_menu/settings_menu.gd
+++ src/game/settings_menu/settings_menu.gd
@@ -7,10 +7,15 @@ signal sfx_changed(enabled: bool)
 
 @export var close_button: BaseButton
 @export var sfx_check_button: CheckButton
+@export var credits_show_button: BaseButton
+@export var credits_menu: Control
+@export var credits_close_button: BaseButton
 
 func _ready() -> void:
 	close_button.pressed.connect(_on_close_button_pressed)
 	sfx_check_button.toggled.connect(_on_sfx_check_button_toggled)
+	credits_show_button.pressed.connect(_on_credits_show_button_pressed)
+	credits_close_button.pressed.connect(_on_credits_close_button_pressed)
 
 func _on_close_button_pressed() -> void:
 	close_requested.emit()
@@ -22,3 +27,11 @@ func set_sfx_eanbled(p_enabled: bool) -> void:
 
 func _on_sfx_check_button_toggled(p_is_toggled: bool) -> void:
 	sfx_changed.emit(p_is_toggled)
+
+
+func _on_credits_close_button_pressed() -> void:
+	credits_menu.visible = false
+
+
+func _on_credits_show_button_pressed() -> void:
+	credits_menu.visible = true
